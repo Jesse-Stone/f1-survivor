3e48298c010cc5e301b80c989463b70f9aabeea4
Jesse Stone <jessecharles@gmail.comm>
2023-03-25 16:33:53 -0600

    icons, and index.html updates

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import DriverList from './components/DriverList'
 import { QueryClient, QueryClientProvider,} from 'react-query'
+import { Typography } from '@mui/material'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
     <div className="App">
+    <img src="/f1_favicon.png" alt="image" width="250" height="250" />
+    <Typography variant='h3'>Testing API Fetching and Caching</Typography>
       <DriverList/>
     </div>
     </QueryClientProvider>
