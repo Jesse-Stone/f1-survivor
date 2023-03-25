36cdd672c40008991c81ee2745c90cb5c0f6dc6a
Jesse Stone <jessecharles@gmail.comm>
2023-03-25 15:29:45 -0600

    mui install

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  // Your theme object here
+});
+
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
