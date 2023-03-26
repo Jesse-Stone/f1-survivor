2d67c0278cf379800ef4b36be0c0c59abe623402
Jesse Stone <jessecharles@gmail.comm>
2023-03-26 11:12:31 -0600

    styling homepage

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,29 @@
 // import './App.css'
-import { QueryClient, QueryClientProvider,} from 'react-query'
-import { Box, Grid, Stack, Typography } from '@mui/material'
-import DriverProfile from './components/DriverProfile'
-import driversData from './data/driversData'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import DriverProfile from './components/DriverProfile';
+import driversData from './data/driversData';
 
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
-    {/* <img src="/f1_favicon.png" alt="image" width="250" height="250" /> */}
-    <>
-    <Typography variant={'h3'} mb={5}>Pick Your Driver</Typography>
-    <Grid justifyContent="center" container spacing={2}>
-        {driversData.Drivers.map(driver => <DriverProfile key={driver.driverId} driver={driver} />)}
-    </Grid>
-    </>
+      {/* <img src="/f1_favicon.png" alt="image" width="250" height="250" /> */}
+      <>
+        <Stack justifyContent={'center'} flexDirection={'row'}>
+          <Typography variant="profile" mb={5} fontSize={50}>
+            PICK YOUR DRIVER
+          </Typography>
+        </Stack>
+        <Grid justifyContent="center" container spacing={2}>
+          {driversData.Drivers.map((driver) => (
+            <DriverProfile key={driver.driverId} driver={driver} />
+          ))}
+        </Grid>
+      </>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
