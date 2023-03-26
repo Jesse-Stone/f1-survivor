e36359621dbf1bc380d7e5c9ea5d67f180c96ea5
Jesse Stone <jessecharles@gmail.comm>
2023-03-26 11:08:18 -0600

    more driver card styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { QueryClient, QueryClientProvider,} from 'react-query'
 import { Box, Grid, Stack, Typography } from '@mui/material'
 import DriverProfile from './components/DriverProfile'
@@ -12,11 +12,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
     {/* <img src="/f1_favicon.png" alt="image" width="250" height="250" /> */}
-    <Stack flexDirection={'row'}>
-
-    {driversData.Drivers.map(driver => <DriverProfile key={driver.driverId} driver={driver} />)}
-
-    </Stack>
+    <>
+    <Typography variant={'h3'} mb={5}>Pick Your Driver</Typography>
+    <Grid justifyContent="center" container spacing={2}>
+        {driversData.Drivers.map(driver => <DriverProfile key={driver.driverId} driver={driver} />)}
+    </Grid>
+    </>
     </QueryClientProvider>
   )
 }
--- vite-project/src/components/DriverProfile.jsx
+++ vite-project/src/components/DriverProfile.jsx
@@ -1,45 +1,73 @@
-import {
-  CardContent,
-  Stack,
-  Divider,
-  CardMedia,
-  Typography,
-  Box
-} from '@mui/material';
+import { Stack, Divider, CardMedia, Typography, Box } from '@mui/material';
 import driversData from '../data/driversData';
 
-const DriverProfile = ({driver}) => {
+const DriverProfile = ({ driver }) => {
   const { givenName, familyName, flag, permanentNumber, team } = driver;
   return (
-    <Stack sx={{ minWidth: 350, backgroundColor: 'white', paddingTop: 1.5, paddingRight: 1, paddingLeft: 1, marginRight:'5px' }}>
+    <Stack
+      sx={{
+        minWidth: 350,
+        backgroundColor: 'white',
+        paddingTop: 1.5,
+        paddingRight: 1,
+        paddingLeft: 1
+        // marginRight: '5px'
+      }}
+    >
       {driver && (
         <>
-        <Divider width={'90%'}/>
-        <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} color="black" sx={{padding:'auto'}}>
-          <Stack direction={'row'} alignItems={'center'}>
-            <Box sx={{backgroundColor:`${team.color}`, height: '35px', width: '5px'}}></Box>
-          <Stack marginLeft={1}>
-            <Typography fontSize={14} fontWeight={100} >
-              {`${givenName}`}
-            </Typography>
-            <Typography variant='profile' fontSize={22}>
-            {`${familyName}`.toUpperCase()}
-            </Typography>
+          <Divider width={'90%'} />
+          <Stack
+            direction={'row'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            color="black"
+            sx={{ padding: 'auto' }}
+          >
+            <Stack direction={'row'} alignItems={'center'}>
+              <Box
+                sx={{
+                  backgroundColor: `${team.color}`,
+                  height: '35px',
+                  width: '5px'
+                }}
+              ></Box>
+              <Stack marginLeft={1}>
+                <Typography fontSize={14} fontWeight={100}>
+                  {`${givenName}`}
+                </Typography>
+                <Typography variant="profile" fontSize={22}>
+                  {`${familyName}`.toUpperCase()}
+                </Typography>
+              </Stack>
             </Stack>
-            </Stack>     
             <img src={flag} height={'60'}></img>
           </Stack>
-          <Divider width={'40%'}/>
+          <Divider width={'40%'} />
           <Typography variant="body2" color="text.secondary">
-              {team.name}
-            </Typography>
+            {team.name}
+          </Typography>
           <CardMedia
             component="img"
             height="200"
-            image = {`${driversData.Drivers.find(drivers => drivers.givenName === `${driver.givenName}`).picture}`}
+            image={`${
+              driversData.Drivers.find(
+                (drivers) => drivers.givenName === `${driver.givenName}`
+              ).picture
+            }`}
             alt={driver.code}
-            sx={{ objectFit: 'scale-down', paddingLeft: 1 }}
+            sx={{ objectFit: 'scale-down', paddingLeft: '20px' }}
           />
+          <Box sx={{
+            position: 'absolute',
+            zIndex: '10',
+            paddingTop: '225px',
+            paddingLeft: '25px'
+          }}>
+            <Typography variant="profile" fontSize={50} color={`${team.color}`}>
+              {permanentNumber}
+            </Typography>
+          </Box>
         </>
       )}
     </Stack>
