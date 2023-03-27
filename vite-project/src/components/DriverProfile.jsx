d53bf53210c7d2a90111bf1ef10643d59bde9b38
Jesse Stone <jessecharles@gmail.comm>
2023-03-27 00:15:31 -0600

    styling

--- vite-project/src/components/DriverProfile.jsx
+++ vite-project/src/components/DriverProfile.jsx
@@ -1,6 +1,7 @@
-import { Stack, Divider, CardMedia, Typography, Box } from '@mui/material';
+import { Stack, Divider, Typography, Box } from '@mui/material';
 import driversData from '../data/driversData';
 import toOrdinalSuffix from '../utils/utils';
+import trophy from '../assets/trophy.png';
 
 const DriverProfile = ({ driver, points, position }) => {
   const { givenName, familyName, flag, permanentNumber, team } = driver;
@@ -11,14 +12,15 @@ const DriverProfile = ({ driver, points, position }) => {
       sx={{
         boxSizing: 'border-box',
         transition: 'all .2s ease-in-out',
-        maxWidth: 250,
+        // maxWidth: 250,
         backgroundColor: 'white',
-        paddingTop: 0.5,
+        // paddingTop: 0.5,
         paddingRight: 1,
         paddingLeft: 1,
         margin: '12px 12px 12px 12px',
         position: 'relative',
         borderRadius: '4px',
+        boxShadow: '8px 4px 4px rgba(192,192,192,0.3)',
         '&:hover': {
           outlineColor: `${team.color}`,
           outlineWidth: '7px',
@@ -27,107 +29,124 @@ const DriverProfile = ({ driver, points, position }) => {
         }
       }}
     >
-      {driver && (
-        <>
-          <Stack
-            direction={'row'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            color="black"
-            sx={{ padding: 'auto' }}
-          >
-            <Stack flexDirection={'row'}>
-            <Typography variant="profile1" fontSize={40} fontWeight={900}>
-              {position}
-            </Typography>
-            <Typography variant="profile2" fontSize={22} fontWeight={400} mt={1}>
-              {toOrdinalSuffix(position)}
-            </Typography>
-            </Stack>
-            <Stack flexDirection={'row'} alignItems={'center'}>
-              <Typography variant="profile2" fontSize={30} fontWeight={400}>
-                {points}
-              </Typography>
-              <Typography
-                variant="profile2"
-                ml={1}
-                fontSize={12}
-                fontWeight={400}
-              >
-                PTS
-              </Typography>
-            </Stack>
-          </Stack>
-          <Divider width={'90%'} />
-          <Stack
-            direction={'row'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            color="black"
-            sx={{ padding: 'auto' }}
-          >
-            <Stack direction={'row'} alignItems={'center'}>
-              <Box
-                sx={{
-                  backgroundColor: `${team.color}`,
-                  height: '35px',
-                  width: '5px'
-                }}
-              ></Box>
-              <Stack marginLeft={1}>
+      <Box
+        sx={{
+          // outlineColor: `${team.color}`,
+          // outlineWidth: '2px',
+          // outlineStyle: 'solid',
+          zIndex: '0',
+        }}
+      >
+        {/* <Box> */}
+        {driver && (
+          <>
+            <Stack
+              direction={'row'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              color="black"
+              sx={{ padding: 'auto' }}
+            >
+              <Stack flexDirection={'row'}>
+                <Typography variant="profile1" fontSize={40} fontWeight={900}>
+                  {position}
+                </Typography>
                 <Typography
                   variant="profile2"
-                  fontSize={14}
-                  fontWeight={100}
-                  mb={-1}
+                  fontSize={22}
+                  fontWeight={400}
+                  mt={1}
                 >
-                  {`${givenName}`}
+                  {toOrdinalSuffix(position)}
                 </Typography>
-                <Typography variant="profile" fontSize={22}>
-                  {`${familyName}`.toUpperCase()}
+              </Stack>
+              <Stack flexDirection={'row'} alignItems={'center'}>
+                <Typography variant="profile2" fontSize={30} fontWeight={400}>
+                  {points}
+                </Typography>
+                <Typography
+                  variant="profile2"
+                  ml={1}
+                  fontSize={12}
+                  fontWeight={400}
+                >
+                  PTS
                 </Typography>
               </Stack>
             </Stack>
-            <img src={flag} height={'60'}></img>
-          </Stack>
-          <Divider width={'40%'} />
-          <Typography variant="profile2" color="text.secondary" fontSize={14}>
-            {team.name}
-          </Typography>
-          <Stack flexDirection={'row'}>
-            <Box component={'img'} src={`${
-              driversData.Drivers.find(
-                (drivers) => drivers.givenName === `${driver.givenName}`
-              ).picture
-            }`} sx= {{height:'150px', paddingLeft: '70px'}}>
-
-            </Box>
-          </Stack>
-          {/* <CardMedia
-            component="img"
-            height="150"
-            image={`${
-              driversData.Drivers.find(
-                (drivers) => drivers.givenName === `${driver.givenName}`
-              ).picture
-            }`}
-            alt={driver.code}
-            sx={{ objectFit: 'scale-down', paddingLeft: '40px' }}
-          /> */}
-          <Box
-            sx={{
-              position: 'absolute',
-              zIndex: '10',
-              bottom: '0',
-              marginBottom: '-15px' //fix this shit
-            }}
-          >
-            <Typography variant="profile" fontSize={55} color={`${team.color}`}>
-              {permanentNumber}
+            <Divider width={'90%'} />
+            <Stack
+              direction={'row'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              color="black"
+              sx={{ padding: 'auto' }}
+            >
+              <Stack direction={'row'} alignItems={'center'}>
+                <Box
+                  sx={{
+                    backgroundColor: `${team.color}`,
+                    height: '35px',
+                    width: '5px'
+                  }}
+                ></Box>
+                <Stack marginLeft={1}>
+                  <Typography
+                    variant="profile2"
+                    fontSize={14}
+                    fontWeight={100}
+                    mb={-1}
+                  >
+                    {`${givenName}`}
+                  </Typography>
+                  <Typography variant="profile" fontSize={22}>
+                    {`${familyName}`.toUpperCase()}
+                  </Typography>
+                </Stack>
+              </Stack>
+              <img src={flag} height={'60'}></img>
+            </Stack>
+            <Divider width={'40%'} />
+            <Typography variant="profile2" color="text.secondary" fontSize={14}>
+              {team.name}
             </Typography>
-          </Box>
-        </>
-      )}
+            <Stack flexDirection={'row'}>
+              <Box
+                component={'img'}
+                src={`${
+                  driversData.Drivers.find(
+                    (drivers) => drivers.givenName === `${driver.givenName}`
+                  ).picture
+                }`}
+                sx={{
+                  height: '150px',
+                  paddingLeft: '70px',
+                  zIndex:'2'
+                  // backgroundImage: `url(${trophy})`,
+                  // backgroundSize: 'contain',
+                  // backgroundRepeat: 'no-repeat'
+                }}
+              ></Box>
+            </Stack>
+            <Box
+              sx={{
+                position: 'absolute',
+                zIndex: '10',
+                bottom: '0',
+                marginBottom: '-15px' //fix this shit
+              }}
+            >
+              <Typography
+                variant="profile"
+                fontSize={55}
+                color={`${team.color}`}
+              >
+                {permanentNumber}
+              </Typography>
+            </Box>
+          </>
+        )}
+      </Box>
     </Stack>
   );
 };
