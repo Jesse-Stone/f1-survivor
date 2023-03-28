2dc6d5964afbe020b060e025d6c1cee581667b72
Jesse Stone <jessecharles@gmail.comm>
2023-03-27 18:07:33 -0600

    react router working

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,87 +5,22 @@ import DriverProfile from './components/DriverProfile';
 import driversData from './data/driversData';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Navbar from './components/NavBar';
-
+import Navbar from './components/Navbar';
+import DriverPicker from './pages/DriverPicker';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        'https://ergast.com/api/f1/2023/results.json'
-      );
-      setData(response.data.MRData.RaceTable.Races);
-    };
-    fetchData();
-  }, []);
-
-  const groupedData = data.reduce((acc, race) => {
-    race.Results.forEach((result) => {
-      const driver = result.Driver.driverId;
-      if (!acc[driver]) {
-        acc[driver] = { driver, points: 0 };
-      }
-      acc[driver].points += parseInt(result.points);
-    });
-    return acc;
-  }, {});
-
-  const sortedData = Object.values(groupedData).sort(
-    (a, b) => b.points - a.points
-  );
-
-  const joinedArray = driversData.Drivers.map((item1) => ({
-    ...item1,
-    ...sortedData.find((item2) => item2.driver === item1.driverId)
-  }));
 
   return (
     <QueryClientProvider client={queryClient}>
-      <>
+      <BrowserRouter>
       <Navbar/>
-        <Stack justifyContent={'center'} flexDirection={'row'}>
-          <Typography variant="profile" fontSize={50}>
-            PICK YOUR DRIVER
-          </Typography>
-        </Stack>
-        <Grid  justifyContent={'center'} container spacing={0} mb={10}>
-          {driversData.Drivers.map((driver) => (
-            <DriverProfile
-              key={driver.driverId}
-              driver={driver}
-              points={
-                joinedArray.find(
-                  (result) => result.driverId === driver.driverId
-                ).points
-              }
-              position ={sortedData.findIndex(obj => obj.driver === driver.driverId) + 1}
-            />
-          ))}
-        </Grid>
-        {/* <div>
-      <h1>Results</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Driver</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedData.map((result) => (
-            <tr key={result.driver}>
-              <td>{result.driver}</td>
-              <td>{result.points}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div> */}
-      </>
+      <Routes>
+        <Route path='/' Component={DriverPicker} exact></Route>
+      </Routes>
+   </BrowserRouter>
     </QueryClientProvider>
   );
 }
