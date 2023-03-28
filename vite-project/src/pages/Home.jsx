aeb5932bd23d201b362312cd6f97b5ce0fb603d4
Jesse Stone <jessecharles@gmail.comm>
2023-03-27 21:48:59 -0600

    typographies moved

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Home = () => {
   return (
     <>
-    <Stack flexDirection={'row'} justifyContent={'center'} alignItems={'center'}>
-      HOME
-    </Stack>
+      <Stack
+        flexDirection={'row'}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+                  <Typography variant="profile" fontSize={30} mt={3} sx = {{ '@media (min-width:550px)': {
+            fontSize: '60px'
+          },
+
+          }}>
+            FORMULA 1  // SURVIVOR
+          </Typography>
+      </Stack>
     </>
-  )
-}
+  );
+};
 
 export default Home;
