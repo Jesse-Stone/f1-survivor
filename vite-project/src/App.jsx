1cea2ef2f8ad525abacc8b0d773761ad425562df
Jesse Stone <jessestone@Jesses-Mac-Studio.local>
2023-03-29 16:34:43 -0600

    about to try protected routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,11 +6,24 @@ import DriverPicker from './pages/DriverPicker';
 import Home from './pages/Home';
 import Standings from './pages/Standings';
 import Login from './pages/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './config/firebase';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const queryClient = new QueryClient();
+
+const WithNav = () => {
+  //hide nav in login flow
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+const WithoutNav = () => <Outlet />;
 
 const App = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -18,16 +31,18 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        {user && <NavBar />}
         <Routes>
-          <Route path="/" Component={Login} exact></Route>
-          {user && (
-            <>
-              <Route path="/home" Component={Home} exact></Route>
-              <Route path="/standings" Component={Standings} exact></Route>
-              <Route path="/makepicks" Component={DriverPicker} exact></Route>
-            </>
-          )}
+          <Route element={<WithoutNav />}>
+            <Route path="/" element={<Login />} />
+            <Route path="*" element={<Login />} />
+          </Route>
+          <Route element={<WithNav />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} exact></Route>
+            <Route path="/standings" element={<Standings />} exact></Route>
+            <Route path="/makepicks" element={<DriverPicker />} exact></Route>
+            <Route path="*" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
