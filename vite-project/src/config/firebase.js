ab0bc647d456be0aad8ee64dfd62fd3ffa9aa35c
Jesse Stone <jessecharles@gmail.comm>
2023-03-31 18:13:34 -0600

    beginning CRUD firestore work

--- vite-project/src/config/firebase.js
+++ vite-project/src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
   
 const firebaseConfig = {
   apiKey: "AIzaSyBwL-Gyjdboz_kP8EIM3EXqLF-9FumXwj4",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
